b93d0205be177adc6bcc8fcfb59e6283cec4c2a8
tyckobra <77719515+tyckobra@users.noreply.github.com>
2022-11-10 13:34:56 +0100

    2022-11-10

--- src/Ball.java
+++ src/Ball.java
@@ -1,2 +1,11 @@
 public class Ball {
+    private Ball(int x, int y) {
+        x = 10;
+        y = 10;
+        int vx;
+        int vy;
+    }
+    public void Ball2(int x, int y) {
+        Ball boll = new Ball(x, y);
+    }
 }
--- src/Pong.java
+++ src/Pong.java
@@ -1,2 +1,14 @@
 public class Pong {
+    private Pong() {
+        Ball b;
+        Paddle p1;
+        Paddle p2;
+    }
+    public void Pong2(){
+        int height = 200;
+        int width = 150;
+    }
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
